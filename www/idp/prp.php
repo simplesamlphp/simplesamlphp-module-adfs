722761114cd1c4fef8aed6acbdd81bfbdb2cdf7f
Tim van Dijen <tvdijen@gmail.com>
2019-09-24 13:56:49 +0200

    Create controller, cleanup old www-files

--- www/idp/prp.php
+++ www/idp/prp.php
@@ -7,23 +7,16 @@
  * @package SimpleSAMLphp
  */
 
-\SimpleSAML\Logger::info('ADFS - IdP.prp: Accessing ADFS IdP endpoint prp');
+namespace SimpleSAML\Module\adfs;
 
-$metadata = \SimpleSAML\Metadata\MetaDataStorageHandler::getMetadataHandler();
-$idpEntityId = $metadata->getMetaDataCurrentEntityID('adfs-idp-hosted');
-$idp = \SimpleSAML\IdP::getById('adfs:'.$idpEntityId);
+use SimpleSAML\Configuration;
+use SimpleSAML\Session;
+use Symfony\Component\HttpFoundation\Request;
 
-if (isset($_GET['wa'])) {
-    if ($_GET['wa'] === 'wsignout1.0') {
-        \SimpleSAML\Module\adfs\IdP\ADFS::receiveLogoutMessage($idp);
-    } elseif ($_GET['wa'] === 'wsignin1.0') {
-        \SimpleSAML\Module\adfs\IdP\ADFS::receiveAuthnRequest($idp);
-    }
-    throw new \Exception("Code should never be reached");
-} elseif (isset($_GET['assocId'])) {
-    // logout response from ADFS SP
-    $assocId = $_GET['assocId']; // Association ID of the SP that sent the logout response
-    $relayState = $_GET['relayState']; // Data that was sent in the logout request to the SP. Can be null
-    $logoutError = null; // null on success, or an instance of a \SimpleSAML\Error\Exception on failure.
-    $idp->handleLogoutResponse($assocId, $relayState, $logoutError);
-}
+$config = Configuration::getInstance();
+$session = Session::getSessionFromRequest();
+$request = Request::createFromGlobals();
+
+$controller = new StatisticsController($config, $session);
+$t = $controller->prp($request);
+$t->send();
