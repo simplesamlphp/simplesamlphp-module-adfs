288f02f94ab21d95564799373bca10ae7fc62eb7
Tim van Dijen <tvdijen@gmail.com>
2022-09-21 17:44:38 +0200

    Fix test-cert directory

--- tests/metadata/adfs-idp-hosted.php
+++ tests/metadata/adfs-idp-hosted.php
@@ -2,12 +2,12 @@
 
 use SimpleSAML\XMLSecurity\TestUtils\PEMCertificatesMock;
 
-$certDir = dirname(__DIR__, 2) . '/vendor/simplesamlphp/xml-security' . PEMCertificatesMock::CERTIFICATE_DIR_RSA;
+$certDir = dirname(__DIR__, 2) . '/vendor/simplesamlphp/xml-security' . PEMCertificatesMock::CERTIFICATE_DIR;
 $metadata['__DYNAMIC:1__'] = [
     'host' => '__DEFAULT__',
     'privatekey_pass' => '1234',
-    'privatekey' =>  $certDir . '/' . PEMCertificatesMock::SELFSIGNED_PRIVATE_KEY_PROTECTED,
-    'certificate' => $certDir . '/' . PEMCertificatesMock::SELFSIGNED_PUBLIC_KEY,
+    'privatekey' =>  $certDir . '/' . PEMCertificatesMock::PRIVATE_KEY,
+    'certificate' => $certDir . '/' . PEMCertificatesMock::PUBLIC_KEY,
     // Some WS-Fed relying parties applications set the session lifetime to the assertion lifetime
     // 'assertion.lifetime' => 3600,
 
