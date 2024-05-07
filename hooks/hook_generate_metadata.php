d20a804b0232da74156c531737dfdb04f35438f5
Ben Martin <monkeyiq@users.sourceforge.net>
2024-05-08 09:11:59 +1000

    A hook to generate metadata

---
+++ hooks/hook_generate_metadata.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\SAML2\Constants as C;
+use SimpleSAML\{Configuration, Utils, Logger, Module};
+use SimpleSAML\Locale\Translate;
+use SimpleSAML\Metadata\MetaDataStorageHandler;
+
+function adfs_hook_generate_metadata(array &$hookinfo): void
+{
+    if( $hookinfo['set'] == 'adfs-idp-hosted' ) {
+
+        $property = $hookinfo['property'];
+        $endpoint = Module::getModuleURL('adfs/idp/prp.php');
+        switch ($property) {
+            case 'SingleSignOnService':
+                $hookinfo['result'] = $endpoint;
+                break;
+                
+            case 'SingleSignOnServiceBinding':
+                $hookinfo['result'] = C::BINDING_HTTP_REDIRECT;
+                break;
+                
+            case 'SingleLogoutService':
+                $hookinfo['result'] = $endpoint;
+                break;
+                
+            case 'SingleLogoutServiceBinding':
+                $hookinfo['result'] = C::BINDING_HTTP_REDIRECT;
+                break;
+        }
+    }
+}
+
