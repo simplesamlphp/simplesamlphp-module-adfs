f4aa39bc0f31d883a79e62d29a1f08eb8309486a
Tim van Dijen <tvdijen@gmail.com>
2024-09-03 21:05:04 +0200

    Fix syntax error

--- src/IdP/MetadataBuilder.php
+++ src/IdP/MetadataBuilder.php
@@ -218,7 +218,7 @@ class MetadataBuilder
                     'encryption',
                     $key['X509Certificate'],
                     $key['name'] ?? null,
-                ));
+                );
             }
         }
 
