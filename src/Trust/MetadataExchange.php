ffd02994729b258be847232a4eee25daee17205d
Tim van Dijen <tvdijen@gmail.com>
2024-09-23 21:27:02 +0200

    Add certificate policies

--- src/Trust/MetadataExchange.php
+++ src/Trust/MetadataExchange.php
@@ -15,8 +15,10 @@ use SimpleSAML\WSSecurity\XML\sp_200507\IncludeTimestamp;
 use SimpleSAML\WSSecurity\XML\sp_200507\IncludeToken;
 use SimpleSAML\WSSecurity\XML\sp_200507\Layout;
 use SimpleSAML\WSSecurity\XML\sp_200507\MustSupportIssuedTokens;
+use SimpleSAML\WSSecurity\XML\sp_200507\MustSupportRefThumbprint;
 use SimpleSAML\WSSecurity\XML\sp_200507\RequireClientEntropy;
 use SimpleSAML\WSSecurity\XML\sp_200507\RequireServerEntropy;
+use SimpleSAML\WSSecurity\XML\sp_200507\RequireThumbprintReference;
 use SimpleSAML\WSSecurity\XML\sp_200507\SignedParts;
 use SimpleSAML\WSSecurity\XML\sp_200507\SignedSupportingTokens;
 use SimpleSAML\WSSecurity\XML\sp_200507\Strict;
@@ -26,6 +28,8 @@ use SimpleSAML\WSSecurity\XML\sp_200507\Trust10;
 use SimpleSAML\WSSecurity\XML\sp_200507\UsernameToken;
 use SimpleSAML\WSSecurity\XML\sp_200507\Wss11;
 use SimpleSAML\WSSecurity\XML\sp_200507\WssUsernameToken10;
+use SimpleSAML\WSSecurity\XML\sp_200507\WssX509V3Token10;
+use SimpleSAML\WSSecurity\XML\sp_200507\X509Token;
 use SimpleSAML\WSSecurity\XML\wsdl\Definitions;
 use SimpleSAML\WSSecurity\XML\wsp\All;
 use SimpleSAML\WSSecurity\XML\wsp\ExactlyOne;
@@ -73,7 +77,160 @@ class MetadataExchange
      */
     private function getPolicies(): array
     {
-        return [$this->getUserNameWSTrustBindingPolicy()];
+        return [
+            $this->getCertificateWSTrustBinding(),
+            $this->getCertificateWSTrustBinding1(),
+            $this->getUserNameWSTrustBindingPolicy(),
+        ];
+    }
+
+
+    /**
+     * This method builds the CertificateWSTrustBinding policy.
+     *
+     * @param \SimpleSAML\WSSecurity\XML\wsp\Policy
+     */
+    private function getCertificateWSTrustBinding(): Policy
+    {
+        $transportBinding = new TransportBinding(
+            elements: [new Policy(
+                children: [
+                    new TransportToken(
+                        elements: [new Policy(
+                            children: [new HttpsToken(false)],
+                        )],
+                    ),
+                    new AlgorithmSuite(
+                        elements: [new Policy(
+                            children: [new Basic256()],
+                        )],
+                    ),
+                    new Layout(
+                        elements: [new Policy(
+                            children: [new Strict()],
+                        )],
+                    ),
+                    new IncludeTimestamp(),
+                ],
+            )],
+        );
+
+        $endorsingSupportingTokens = new EndorsingSupportingTokens(
+            elements: [new Policy(
+                children: [
+                    new X509Token(
+                        elts: [new Policy(
+                            children: [
+                                new RequireThumbprintReference(),
+                                new WssX509V3Token10(),
+                            ],
+                        )],
+                        namespacedAttributes: [
+                            new XMLAttribute(
+                                C::NS_SEC_POLICY_11,
+                                'sp',
+                                'IncludeToken',
+                                IncludeToken::AlwaysToRecipient->value,
+                            ),
+                        ],
+                    ),
+                    new RsaToken(
+                        namespacedAttributes: [
+                            new XMLAttribute(C::NS_SEC_POLICY_11, 'sp', 'IncludeToken', IncludeToken::Never->value),
+                            new XMLAttribute(C::NS_POLICY, 'wsp', 'Optional', 'true'),
+                        ],
+                    ),
+                    new SignedParts(
+                        header: [
+                            new Header(
+                                namespace: C::NS_ADDR_200508,
+                                name: 'To',
+                            ),
+                        ],
+                    ),
+                ],
+            )],
+        );
+
+        $wss11 = new Wss11(
+            elements: [new Policy(
+                children: [
+                    new MustSupportRefThumbprint(),
+                ],
+            )],
+        );
+
+        $trust10 = new Trust10(
+            elements: [new Policy(
+                children: [
+                    new MustSupportIssuedTokens(),
+                    new RequireClientEntropy(),
+                    new RequireServerEntropy(),
+                ],
+            )],
+        );
+
+        $usingAddressing = new UsingAddressing();
+
+        return new Policy(
+            Id: new XMLAttribute(C::NS_SEC_UTIL, 'wsu', 'Id', 'CertificateWSTrustBinding_IWSTrustFeb2005Async_policy'),
+            operatorContent: [new ExactlyOne(
+                operatorContent: [new All(
+                   children: [
+                       $transportBinding,
+                       $endorsingSupportingTokens,
+                       $wss11,
+                       $trust10,
+                       $usingAddressing,
+                   ],
+                )],
+            )],
+        );
+    }
+
+
+    /**
+     * This method builds the CertificateWSTrustBinding1 policy.
+     *
+     * @param \SimpleSAML\WSSecurity\XML\wsp\Policy
+     */
+    private function getCertificateWSTrustBinding1(): Policy
+    {
+        $transportBinding = new TransportBinding(
+            elements: [new Policy(
+                children: [
+                    new TransportToken(
+                        elements: [new Policy(
+                            children: [new HttpsToken(true)],
+                        )],
+                    ),
+                    new AlgorithmSuite(
+                        elements: [new Policy(
+                            children: [new Basic256()],
+                        )],
+                    ),
+                    new Layout(
+                        elements: [new Policy(
+                            children: [new Strict()],
+                        )],
+                    ),
+                ],
+            )],
+        );
+
+        $usingAddressing = new UsingAddressing();
+
+        return new Policy(
+            Id: new XMLAttribute(C::NS_SEC_UTIL, 'wsu', 'Id', 'CertificateWSTrustBinding_IWSTrustFeb2005Async1_policy'),
+            operatorContent: [new ExactlyOne(
+                operatorContent: [new All(
+                   children: [
+                       $transportBinding,
+                       $usingAddressing,
+                   ],
+                )],
+            )],
+        );
     }
 
 
