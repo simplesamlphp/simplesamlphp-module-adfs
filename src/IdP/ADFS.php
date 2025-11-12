844e851c3fa00c4f50eb2e6942801234fbdc8831
Mark Kaulertz <33390109+m0ark@users.noreply.github.com>
2025-11-12 20:02:24 +0100

    Update getLogoutURL to accept nullable relayState parameter to fix iframe logout scenarios where no relayState is available (#37)

--- src/IdP/ADFS.php
+++ src/IdP/ADFS.php
@@ -668,15 +668,19 @@ class ADFS
      *
      * @param \SimpleSAML\IdP $idp
      * @param array<mixed> $association
-     * @param string $relayState
+     * @param string|null $relayState
      * @return string
      */
-    public static function getLogoutURL(IdP $idp, array $association, string $relayState): string
+    public static function getLogoutURL(IdP $idp, array $association, ?string $relayState = null): string
     {
         $metadata = MetaDataStorageHandler::getMetadataHandler();
         $spMetadata = $metadata->getMetaDataConfig($association['adfs:entityID'], 'adfs-sp-remote');
+        $params = ['assocId' => urlencode($association['id'])];
+        if ($relayState !== null) {
+            $params['relayState'] = urlencode($relayState);
+        }
         $returnTo = Module::getModuleURL(
-            'adfs/idp/prp.php?assocId=' . urlencode($association["id"]) . '&relayState=' . urlencode($relayState),
+            'adfs/idp/prp.php', $params
         );
         return $spMetadata->getValue('prp') . '?wa=wsignoutcleanup1.0&wreply=' . urlencode($returnTo);
     }
